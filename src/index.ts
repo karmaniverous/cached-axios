66f86ab71d3a7196cf5cfa6efc747d695af711ea
Jason Williscroft <me@karmanivero.us>
2025-08-29 16:52:17 +0800

    transfer from other repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-/**
- * This is the main entry point for the library.
- *
- * @packageDocumentation
- */
+export { withMutation, withQuery } from './cache';
+export { cachedAxios } from './cachedAxios';
+export type { BuiltNode, ConfigInput, Id, Tag } from './config';
+export { buildConfig, ConfigInputSchema } from './config';
+export { makeCacheHelpers } from './factory';
+export {
+  type OrvalBodyType,
+  type OrvalErrorType,
+  orvalMutator,
+} from './mutator';
 
-export { foo, type FooTarget } from './foo';
+// Re-export axios types for generated code compatibility
+export type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+// Re-export ACI types used in our local augmentations
+export type {
+  CacheProperties,
+  CacheRequestConfig,
+} from 'axios-cache-interceptor';
