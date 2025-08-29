e53f6ccecd267d2f156b2b81104f70df919a3f0d
Jason Williscroft <me@karmanivero.us>
2025-08-29 17:03:42 +0800

    ts lint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,6 @@ import eslint from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
-import tsDocPlugin from 'eslint-plugin-tsdoc';
-import vitestPlugin from 'eslint-plugin-vitest';
 import { dirname } from 'path';
 import tseslint from 'typescript-eslint';
 import { fileURLToPath } from 'url';
@@ -12,42 +10,32 @@ const tsconfigRootDir = dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config(
   {
-    ignores: [
-      '.rollup.cache/**/*',
-      'coverage/**/*',
-      'dist/**/*',
-      'docs/**/*',
-      'node_modules/**/*',
-    ],
+    ignores: ['**/.tsbuild/**', 'node_modules/**', 'stan/**'],
   },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   prettierConfig,
   {
-    ...vitestPlugin.configs.recommended,
-    files: ['**/*.test.ts'],
-  },
-  {
     languageOptions: {
       parserOptions: {
-        project: true,
+        projectService: true,
         tsconfigRootDir,
       },
     },
     plugins: {
-      prettierPlugin,
+      prettier: prettierPlugin,
       'simple-import-sort': simpleImportSortPlugin,
-      tsdoc: tsDocPlugin,
     },
     rules: {
       '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/require-await': 'off',
       'no-unused-vars': 'off',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'tsdoc/syntax': 'warn',
     },
   },
 );
